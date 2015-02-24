49cc9ae98e752f47035af163f5e0a1d07ae2cbc2
omer schleifer <omer.schliefer@picscout.com>
2015-02-24 19:38:56 +0200

    solution test

--- dependency/src/test/java/picscout/depend/dependency/SolutionMapperTest.java
+++ dependency/src/test/java/picscout/depend/dependency/SolutionMapperTest.java
@@ -16,52 +16,74 @@ import picscout.depend.dependency.interfaces.IProjectDescriptor;
 import picscout.depend.dependency.interfaces.ISolution;
 
 public class SolutionMapperTest {
-	
+
 	private SolutionMapper mapper;
 	private ArrayList<IProject> projects = new ArrayList<IProject>();
 	private IProject aProj;
 	private IProject bProj;
 	private IProject cProj;
 	private IProject dProj;
-	private ISolution aSol;  
-	private ISolution bSol; 
-	private ISolution cSol; 
-	private ISolution dSol; 
+	private ISolution aSol;
+	private ISolution bSol;
+	private ISolution cSol;
+	private ISolution dSol;
 	private List<ISolution> result;
-	
+
 	@Before
-	public void init(){
-		ProjectDependencyMapper projectMapper = Mockito.mock(ProjectDependencyMapper.class);
+	public void init() {
+		ProjectDependencyMapper projectMapper = Mockito
+				.mock(ProjectDependencyMapper.class);
 		mapper = new SolutionMapper(projectMapper);
-		 aProj = TestProjectsGenerator.createParoject("a", projects);
-		 bProj = TestProjectsGenerator.createParoject("b", projects);
-		 cProj = TestProjectsGenerator.createParoject("c", projects);
-		 dProj = TestProjectsGenerator.createParoject("d", projects);
-	
-    	Mockito.when(projectMapper.getProjectsThatDepeantOn(aProj.getDescriptor())).thenReturn(null);
-		Mockito.when(projectMapper.getProjectsThatDepeantOn(bProj.getDescriptor())).thenReturn(	new ArrayList<IProjectDescriptor>(Arrays.asList(aProj.getDescriptor())));
-		Mockito.when(projectMapper.getProjectsThatDepeantOn(cProj.getDescriptor())).thenReturn(	Arrays.asList(dProj.getDescriptor(),bProj.getDescriptor(),aProj.getDescriptor()));
-		Mockito.when(projectMapper.getProjectsThatDepeantOn(dProj.getDescriptor())).thenReturn(	Arrays.asList(cProj.getDescriptor(),bProj.getDescriptor(),aProj.getDescriptor()));
-		
-		 aSol = TestProjectsGenerator.createSoution("aSolution", Arrays.asList(aProj.getDescriptor(), bProj.getDescriptor()));
-		 bSol = TestProjectsGenerator.createSoution("bSolution", Arrays.asList(bProj.getDescriptor(), cProj.getDescriptor(), dProj.getDescriptor()));
-		 cSol = TestProjectsGenerator.createSoution("cSolution", Arrays.asList(aProj.getDescriptor(), dProj.getDescriptor()));
-		 dSol = TestProjectsGenerator.createSoution("dSolution", Arrays.asList(cProj.getDescriptor()));
-		
-		 mapper.add(aSol);
-		 mapper.add(bSol);
-		 mapper.add(cSol);
-		 mapper.add(dSol);
+		aProj = TestProjectsGenerator.createParoject("a", projects);
+		bProj = TestProjectsGenerator.createParoject("b", projects);
+		cProj = TestProjectsGenerator.createParoject("c", projects);
+		dProj = TestProjectsGenerator.createParoject("d", projects);
+
+		Mockito.when(
+				projectMapper.getProjectsThatDepeantOn(aProj.getDescriptor()))
+				.thenReturn(null);
+		ArrayList<IProjectDescriptor> list = new ArrayList<IProjectDescriptor>();
+		list.add(aProj.getDescriptor());
+		Mockito.when(
+				projectMapper.getProjectsThatDepeantOn(bProj.getDescriptor()))
+				.thenReturn(list);
+		list = new ArrayList<IProjectDescriptor>();
+		list.add(dProj.getDescriptor());
+		list.add(bProj.getDescriptor());
+		list.add(aProj.getDescriptor());
+		Mockito.when(
+				projectMapper.getProjectsThatDepeantOn(cProj.getDescriptor()))
+				.thenReturn(list);
+		list = new ArrayList<IProjectDescriptor>();
+		list.add(cProj.getDescriptor());
+		list.add(bProj.getDescriptor());
+		list.add(aProj.getDescriptor());
+		Mockito.when(
+				projectMapper.getProjectsThatDepeantOn(dProj.getDescriptor()))
+				.thenReturn(list);
+
+		aSol = TestProjectsGenerator.createSoution("aSolution",
+				Arrays.asList(aProj.getDescriptor(), bProj.getDescriptor()));
+		bSol = TestProjectsGenerator.createSoution("bSolution", Arrays.asList(
+				bProj.getDescriptor(), cProj.getDescriptor(),
+				dProj.getDescriptor()));
+		cSol = TestProjectsGenerator.createSoution("cSolution",
+				Arrays.asList(aProj.getDescriptor(), dProj.getDescriptor()));
+		dSol = TestProjectsGenerator.createSoution("dSolution",
+				Arrays.asList(cProj.getDescriptor()));
+
+		mapper.add(aSol);
+		mapper.add(bSol);
+		mapper.add(cSol);
+		mapper.add(dSol);
 	}
-	
+
 	@Test
-	public void testNotExistingProject()	{
-		result = mapper.getSolutionsByProject(Mockito.mock(IProjectDescriptor.class));
-		Assert.assertNull("No solutions depend on bogus project", result);
-	
+	public void testNotExistingProject() {
+		result = mapper.getSolutionsByProject(Mockito
+				.mock(IProjectDescriptor.class));
+		Assert.assertTrue("No solutions depend on bogus project", result.isEmpty());
+
 	}
-	
-	
-	
 
 }
