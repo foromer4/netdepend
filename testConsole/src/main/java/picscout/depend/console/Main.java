1f20ab6f27358824384fb2323d4c792744bf0966
omer schleifer <omer.schliefer@picscout.com>
2015-04-08 17:30:39 +0300

    arrange code in console

--- testConsole/src/main/java/picscout/depend/console/Main.java
+++ testConsole/src/main/java/picscout/depend/console/Main.java
@@ -1,13 +1,16 @@
 package picscout.depend.console;
 
+import picscout.depend.dependency.main.Runner;
+
 public class Main {
 
 	public static void main(String[] args) {
 
 		//Test jar run
-		 System.out.println("Hello console");
+		 System.out.println("Going to run calc dependencies");
  
-		
+		 Runner runner = new Runner();
+		 runner.CalculateDependencies();
 	}
 
 }
