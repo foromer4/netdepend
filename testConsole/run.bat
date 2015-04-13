7679cb91382a91ba305c87db1081f1a851f8509b
omer schleifer <omer.schliefer@picscout.com>
2015-04-13 19:27:20 +0300

    add log4j config to console

--- testConsole/run.bat
+++ testConsole/run.bat
@@ -1,5 +1,5 @@
 c:
 cd\
 cd C:\git\netdepend\testConsole\target
-java -jar console-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+java -jar -Dlog4j.configuration=file:"C:\git\netdepend\testConsole\log4j2.xml"  console-0.0.1-SNAPSHOT-jar-with-dependencies.jar 
 pause
