c503d015794810252764a0c44346f59869fb0284
omer schleifer <omer.schliefer@picscout.com>
2015-04-14 08:20:07 +0300

    pass params to console

--- testConsole/src/main/java/picscout/depend/console/Main.java
+++ testConsole/src/main/java/picscout/depend/console/Main.java
@@ -1,16 +1,30 @@
 package picscout.depend.console;
 
+import java.util.Arrays;
+import java.util.List;
+
+import picscout.depend.dependency.interfaces.ISolution;
 import picscout.depend.dependency.main.Runner;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		//Test jar run
-		 System.out.println("Going to run calc dependencies");
- 
-		 Runner runner = new Runner();
-		 runner.CalculateDependencies();
+		// Test jar run
+
+		Runner runner = new Runner();
+		if (args.length == 0) {
+			System.out.println("Going to run calc dependencies");
+			runner.CalculateDependencies();
+		}
+		
+		else {
+			System.out.println("Going to run calc dependent solutions");
+			List<ISolution> result = runner.getSolutionsThatDependOnSolutionsByNames(Arrays.asList(args));
+			for(ISolution sol : result) {
+				System.out.println(sol.getName());				
+			}
+		}
 	}
 
 }
