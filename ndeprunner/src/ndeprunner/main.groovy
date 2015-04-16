55dc214f971ea89912c33617216ce2d51125d4ad
omer schleifer <omer.schliefer@picscout.com>
2015-04-16 07:45:32 +0300

    add groovy runner

---
+++ ndeprunner/src/ndeprunner/main.groovy
@@ -0,0 +1,25 @@
+import picscout.depend.dependency.main.Runner;
+import picscout.depend.dependency.interfaces.ISolution;
+public class Wrapper {
+   
+    
+    public static void main(String[] args) {
+    
+   
+      // args = {"Picscout.Inf.profiling"};
+   
+        Runner runner = new Runner();
+        if (args.length == 0) {
+            System.out.println("Going to run calc dependencies");
+            runner.CalculateDependencies();
+        }
+        
+        else {
+            System.out.println("Going to run calc dependent solutions");
+            List<ISolution> result = runner.getSolutionsThatDependOnSolutionsByNames(Arrays.asList(args));
+            for(ISolution sol : result) {
+                System.out.println(sol.getName());                
+            }
+        }
+    }
+    }
