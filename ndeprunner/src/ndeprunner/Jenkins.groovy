c806fb48c4aba2bc1512fe4ad1384e7b2c6c9123
omer schleifer <omer.schliefer@picscout.com>
2015-05-11 19:26:45 +0300

    get solution names

--- ndeprunner/src/ndeprunner/Jenkins.groovy
+++ ndeprunner/src/ndeprunner/Jenkins.groovy
@@ -13,18 +13,12 @@ def static map = [
         def repo = 'infrastructure' //args[0]
         def inputJobs = 'PicScout_Inf_MessagingFramework;PicScout_Inf_Messaging;' //args[1]
         
-        println 'going to calc depenencies' + 'repo is:' + repo + 'jobs to calc for are: ' + inputJobs      
-      
-       def  inputsolutionNames
-       inputsolutionNames = parseJobs(inputJobs)
+        println 'going to calc depenencies' + 'repo is:' + repo + 'jobs to calc for are: ' + inputJobs    
+         def  inputsolutionNames  
+        inputsolutionNames = parseJobs(inputJobs)  
+         def dependentsolutionNames   
+    dependentsolutionNames = getDependentSolutionNames(inputsolutionNames)
     
-    
-     Runner runner = new Runner();
-    List<ISolution> result = runner.getSolutionsThatDependOnSolutionsByNames(Arrays.asList(inputsolutionNames));
-    println 'got results:' + result
-            for(ISolution sol : result) {
-                System.out.println(sol.getName());                
-            }
     
 }
 
@@ -33,11 +27,20 @@ static parseJobs(inputJobs) {
         def inputsolutionNames = ''
         for(job in inputJobsList) {
         inputsolutionNames = HandleJob(job, inputsolutionNames)
-          println inputsolutionNames
-  
+          println inputsolutionNames  
 }
 }
 
+static geDependentSolutionNames(inputsolutionNames) {
+ ArrayList<String>  dependentsolutionNames = new  ArrayList<String>()
+ Runner runner = new Runner();
+    List<ISolution> result = runner.getSolutionsThatDependOnSolutionsByNames(Arrays.asList(inputsolutionNames));  
+            for(ISolution sol : result) {
+                System.out.println(sol.getName());
+               dependentsolutionNames.add(sol.getName())                
+            }
+           return dependentsolutionNames
+            }
 
 
 
