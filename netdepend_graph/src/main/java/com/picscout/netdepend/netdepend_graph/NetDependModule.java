6d339a4102840b488d43ecc4df1fcbb054a82892
omer schleifer <omer.schliefer@picscout.com>
2015-06-07 12:41:55 +0300

    add comment

--- netdepend_graph/src/main/java/com/picscout/netdepend/netdepend_graph/NetDependModule.java
+++ netdepend_graph/src/main/java/com/picscout/netdepend/netdepend_graph/NetDependModule.java
@@ -1,9 +1,14 @@
 package com.picscout.netdepend.netdepend_graph;
 
+import java.security.spec.ECGenParameterSpec;
+
 import org.apache.log4j.Logger;
+import org.apache.xerces.dom.ElementDefinitionImpl;
+import org.bouncycastle.operator.InputExpanderProvider;
 
 import com.google.common.collect.Lists;
 import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
 import com.google.inject.Key;
 import com.google.inject.Module;
 import com.google.inject.multibindings.MapBinderBinding;
@@ -13,24 +18,45 @@ import com.google.inject.multibindings.MultibindingsTargetVisitor;
 import com.google.inject.spi.DefaultBindingTargetVisitor;
 import com.google.inject.spi.Element;
 import com.google.inject.spi.Elements;
+import com.google.inject.util.Modules;
+import com.picscout.depend.dependency.classes.ProjectBuilder;
+import com.picscout.depend.dependency.interfaces.IProjectBuilder;
 
 import hudson.Extension;
+import hudson.plugins.depgraph_view.model.graph.DependencyGraphEdgeProvider;
+import hudson.plugins.depgraph_view.model.graph.DependencyGraphModule;
 import hudson.plugins.depgraph_view.model.graph.EdgeProvider;
 
+/**
+ * Module used to inject new edge provider. still not usable, see: {@link:
+ * https://issues.jenkins-ci.org/browse/JENKINS-28773?jql=project%20%3D%20
+ * JENKINS
+ * %20AND%20status%20in%20(Open%2C%20%22In%20Progress%22%2C%20Reopened)%20
+ * AND%20component%20%3D%20%27depgraph-view-plugin%27}
+ * 
+ * @author OSchliefer
+ *
+ */
 @Extension
 public class NetDependModule extends AbstractModule {
 
 	private final static Logger LOG = Logger.getLogger(NetDependModule.class
 			.getName());
 
+	public NetDependModule() {
+		LOG.info("NetDependModule override was called. "
+				+ Thread.currentThread().getStackTrace());
+
+	}
+
 	@Override
 	protected void configure() {
 
-		LOG.info("NetDependModule configure was called. " + Thread.currentThread().getStackTrace());
+		LOG.info("NetDependModule configure was called. "
+				+ Thread.currentThread().getStackTrace());
 		Multibinder<EdgeProvider> edgeProviderMultibinder = Multibinder
 				.newSetBinder(binder(), EdgeProvider.class);
 		edgeProviderMultibinder.addBinding().to(EdgeProviderImpl.class);
 	}
 
 }
-
